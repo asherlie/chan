f11162e5e31f330c8c1799aee2ebc5c721ad22fd
Asher Lieber <asherlie@buffalo.edu>
2016-11-15 01:31:59 -0500

    added some error handling

--- save_pics.py
+++ save_pics.py
@@ -1,8 +1,13 @@
 import chan
 import sys
 
-
+if len(sys.argv) != 2:
+	raise Exception('too few or too many args. get ur shit together.')
 c = chan.Chan()
-c.save_pics(sys.argv[1].split('#')[0].split('/')[3], sys.argv[1].split('#')[0].split('/')[5], '4chan')
-
-
+split = sys.argv[1].split('#')[0].split('/')
+if len(split) < 6:
+	raise Exception('malformed url')
+board = split[3]
+post_id = split[5]
+directory = '4chan' #default dir to save in
+c.save_pics(board, post_id, directory)
